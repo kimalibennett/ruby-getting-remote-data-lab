1eccde99c747df7fd1aa6def7905ccd7a6b5acd3
kimalibennett <aminbennett@gmail.com>
2020-12-20 12:49:16 -0600

    commit tree 30b3d7a1ced0b154004de096c91846cd94ed2b1f

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,17 @@
 # Write your code here
+class GetRequester 
+
+    @@all = []
+    def intialize(url)
+        url = google.com
+        
+    end
+
+    def get_respones_body
+        GET.url
+    end
+
+    def parse_json
+
+    end
+end
